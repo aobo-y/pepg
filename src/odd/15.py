4a9c5a1d2e5be8802fa40203f800233a3c126ea9
Yang Aobo <yangaobo@gmail.com>
2016-04-03 13:20:36 +0800

    problem 15 more output

--- src/odd/15.py
+++ src/odd/15.py
@@ -10,8 +10,14 @@ for i in range(0, destX + 1):
     for j in range(0, destY + 1):
         storage[i].append(0);
 
+numberOfCall = 0
+numberOfAdd = 0
 def numberOfRountes(x, y, destX, destY):
     global storage
+    global numberOfCall
+    global numberOfAdd
+
+    numberOfCall += 1
 
     if x == destX or y == destY:
         return 1
@@ -20,10 +26,13 @@ def numberOfRountes(x, y, destX, destY):
         return storage[x][y]
 
     routes = numberOfRountes(x + 1, y, destX, destY) + numberOfRountes(x, y + 1, destX, destY)
-
+    numberOfAdd += 1
     storage[x][y] = routes
     return routes
 
 routes = numberOfRountes(x, y, destX, destY)
-print(storage)
+
+# print(storage)
+print("numberOfAdd " + str(numberOfAdd))
+print("numberOfCall " + str(numberOfCall))
 print(routes)
